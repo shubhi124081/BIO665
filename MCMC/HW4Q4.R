6ec5b7177c4548890981bbf8f5e5b867d3024c9c
mes114 <mes114@duke.edu>
2020-02-23 22:22:10 -0500

    even more q4 (hey that rhymed)

--- MCMC/HW4Q4.R
+++ MCMC/HW4Q4.R
@@ -16,52 +16,84 @@ library(rjags)
 source('../clarkfunctions2020.R')
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# SCRATCH
-
-data  <- read.table('FACEtrees.txt',header=T)
-form  <- as.formula(cones ~ nfert*trt + diam)
-X     <- model.matrix(form, data=data)
-Y     <- model.frame(form, data=data)$cones
-n     <- nrow(X)
-p     <- ncol(X)
-# beta  <- matrix(c(-3,3),p,1)           # parameter vector
-file <- "regModel.txt" 
-
-cat("model{
-
-  for (i in 1:n){
-    Y[i] ~ dpois(lambda[i])                  
-    lambda[i] <- exp( inprod(beta[],X[i,]) ) 
-  }
-  beta[1] ~ dnorm(0, .1)
-  beta[2] ~ dunif(0, 10)
-  beta[3] ~ dunif(0, 10)
-  beta[4] ~ dnorm(0, .1)
-  beta[5] ~ dnorm(0, .1)
-}", file = file)
-
-outjags <- jags.model(file = file, 
-                      data = list(Y=Y,n=nrow(X),X = X))
-update(outjags, 200)                # burnin 
-
-outjags <- coda.samples(outjags, variable.names="beta", 
-                        n.iter=2000)
-
-
-tmp <- summary(outjags)
-print(tmp$statistics)
-
-traceplot(outjags)
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # params
 # p = freq of allele A; f = inbreeding coef
 # pmake out = c(paa, pab, pbb)
+# minf out = minimum amt inbreeding based on allele A freq
 
 pmake(c(.5,0)) #no inbreeding
+pmake(c(.5,0.9)) #lots of inbreeding
+pmake(c(.5,-0.9)) #negative inbreeding
+minf(.5)
 
-n <- c
+# Simulation
 
+# params
+npop <- 20               #no. populations
+p  <- 0.6                #frequency of allele a
+f  <- -.2                #inbreeding coefficient
+pr <- pmake(c(p ,f))     #values for (Paa,Pab,Pbb)
+
+# Create data
+popsizes <- c(5, 10, 20)        #population sizes
+for (n in popsizes) {
+  y <- rmultinom(npop,n,pr)    #n random vectors of size m
+}
+
+# Gibbs params
+g1       <- 1   #beta parameters for p
+g2       <- 1
+fg       <- f   #initial value of f
+priorF   <- 0   #mean and sd for f
+priorFSD <- 1
+ng     <- 5000
+nchain <- 10
+nt     <- 500
+thin   <- round(seq(1, ng, length=nt))  
+pgibbs <- matrix(0,nt,nchain)
+fgibbs <- pgibbs
+
+# Beta chains
+for(j in 1:nchain){
+  
+  pg <- rbeta(1,1,1)   #draw initial values from prior
+  lg <- minf(pg)  # lower limit for f
+  fg <- .tnorm(1,lg,1,priorF,priorFSD) #random draw for f
+  k <- 0 #count variable
+  
+  for(g in 1:ng){
+    
+    pf <- update_pf()
+    pg <- pf$pg
+    fg <- pf$fg
+    
+    if(g %in% thin){
+      k <- k + 1
+      pgibbs[k,j] <- pg
+      fgibbs[k,j] <- fg
+    }
+  }
+}
+
+# Graph of all chains
+par(mfrow=c(2,1), mar=c(4,4,1,1),bty='n')
+plot(fgibbs[,1],type='l',ylim=c(-1,1), ylab='f')
+for(j in 2:nchain)lines(fgibbs[,j])
+abline(h=f, lty=2)
+plot(pgibbs[,1],type='l',ylim=c(0,1), ylab='p')
+for(j in 2:nchain)lines(pgibbs[,j])
+abline(h=p, lty=2)
+
+# Graph of joint 
+par(mfrow=c(1,1), bty='n')
+pseq <- seq(.01,.99,length=100)
+plot(pgibbs,fgibbs,xlim=c(0,1),ylim=c(-1,1), cex=.2,xlab='p',
+     ylab='f')
+lines(pseq,minf(pseq))
+
+.processPars(as.vector(fgibbs[-c(1:100),]),xtrue=f,DPLOT=T)
+# red line is true; does pretty well
 
 
 
