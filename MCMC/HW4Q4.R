50bdb52969d098d906b9de81ec27bb75904ebb5e
mes114 <mes114@duke.edu>
2020-02-24 11:30:05 -0500

    all compiled into rmd

--- MCMC/HW4Q4.R
+++ MCMC/HW4Q4.R
@@ -27,9 +27,16 @@ pmake(c(.5,0.9)) #lots of inbreeding
 pmake(c(.5,-0.9)) #negative inbreeding
 minf(.5)
 
+#----------------------------------------------------------------------------
+## QUESTION 4
+
 # Simulation
 
 # params
+# p = freq of allele A; f = inbreeding coef
+# pmake out = c(paa, pab, pbb)
+# minf out = minimum amt inbreeding based on allele A freq
+
 npop <- 20               #no. populations
 p  <- 0.6                #frequency of allele a
 f  <- -.2                #inbreeding coefficient
@@ -37,18 +44,18 @@ pr <- pmake(c(p ,f))     #values for (Paa,Pab,Pbb)
 
 # Create data
 popsizes <- c(5, 10, 20)        #population sizes
-for (n in popsizes) {
-  y <- rmultinom(npop,n,pr)    #n random vectors of size m
+ylist <- list(y1=NULL, y2=NULL, y3=NULL)
+for (i in 1:len(popsizes)) {
+  ylist[i] <- rmultinom(npop,popsizes[i],pr)    #n random vectors of size m
 }
 
 # Gibbs params
-g1       <- 1   #beta parameters for p
-g2       <- 1
+g1 <- g2 <- 1   #beta shape params
 fg       <- f   #initial value of f
 priorF   <- 0   #mean and sd for f
 priorFSD <- 1
 ng     <- 5000
-nchain <- 10
+nchain <- 1
 nt     <- 500
 thin   <- round(seq(1, ng, length=nt))  
 pgibbs <- matrix(0,nt,nchain)
@@ -57,14 +64,13 @@ fgibbs <- pgibbs
 # Beta chains
 for(j in 1:nchain){
   
-  pg <- rbeta(1,1,1)   #draw initial values from prior
-  lg <- minf(pg)  # lower limit for f
-  fg <- .tnorm(1,lg,1,priorF,priorFSD) #random draw for f
-  k <- 0 #count variable
+  k <- 0                # count variable 
+  pg <- rbeta(1,g1,g2)  # draw initial values from prior
+  lg <- minf(pg)        # lower limit for f
+  fg <- .tnorm(1,lg,1,priorF,priorFSD) # random draw for f
   
   for(g in 1:ng){
-    
-    pf <- update_pf()
+    pf <- update_pf()   #from clarkfunctions2020.R
     pg <- pf$pg
     fg <- pf$fg
     
@@ -91,11 +97,7 @@ pseq <- seq(.01,.99,length=100)
 plot(pgibbs,fgibbs,xlim=c(0,1),ylim=c(-1,1), cex=.2,xlab='p',
      ylab='f')
 lines(pseq,minf(pseq))
-
 .processPars(as.vector(fgibbs[-c(1:100),]),xtrue=f,DPLOT=T)
-# red line is true; does pretty well
-
-
 
 
 
--- MCMC/Hw4Q1.R
+++ MCMC/Hw4Q1.R
@@ -1,5 +1,45 @@
-source('/Users/zeyi/Documents/PhD/Classes/Bayesian_inference/JimClarkFunctions2020.R')
+# LIBRARIES
+rm(list=ls())
+library(rjags)
+source('../clarkfunctions2020.R')
+getMu <- function(sigma, y, M, m){ # conditional distributions
+  
+  n <- length(y)
+  V <- 1/( n/sigma + 1/M )
+  v <- sum(y)/sigma + m/M
+  rnorm(1, V*v, sqrt(V))
+}
+getSigma <- function(mu, y, s1, s2){
+  
+  n  <- length(y)
+  u1 <- s1 + n/2
+  u2 <- s2 + 1/2*sum( (y - mu)^2 )
+  1/rgamma(1, u1, u2)
+}
 
+n  <- 10                          # simulate data
+s  <- 1
+mu <- -2
+y  <- rnorm(n, mu, sqrt(s))
+
+m  <- 0                            # prior parameter values
+M  <- 1
+s1 <- s2 <- 1
+
+mg <- 0
+sg <- 2
+
+G <- 1000
+chains <- matrix(NA,G,2)
+
+for(g in 1:G){
+  mg <- getMu(sg, y, M, m)
+  sg <- getSigma(mg, y, s1, s2)
+  chains[g,] <- c(mg, sg)
+}
+qc <- apply(chains,2,quantile,c(.025,.975))
+
+# Z's code
 summ <- data.frame(mean = colMeans(chains), 
                    median = apply(chains,2,median), 
                    Quantile1 = apply(chains,2,quantile,c(.025,.975))[1,],
